01a3221a6a75402348a3c46391f66a52c22bff9b
Ivan Sadykov <grandpajok@gmail.com>
2020-09-14 00:49:09 +0600

    feat: create solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+    if (!array || !Array.isArray(array) || !array.length) {
+        return 0;
+    }
+    return Math.min.apply(null, array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || !Array.isArray(array) || !array.length) {
+        return 0;
+    }
+    return Math.max.apply(null, array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+/**
+ *
+ * @param {*} array
+ */
+exports.avg = function avg(array) {
+    if (!array || !Array.isArray(array) || !array.length) {
+        return 0;
+    }
+    return array.reduce((avg, n) => avg + n, 0) / array.length;
+};
